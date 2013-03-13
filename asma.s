8edf2e7833f85408513e21968c4ec497cdbd78b3
David Vallner <millimoose@gmail.com>
2013-03-13 17:13:51 +0100

    Use boilerplate from test server

--- asma.s
+++ asma.s
@@ -1,22 +1,31 @@
 
-.section .rodata,
+# code end
+.file   "asma.s"
+.globl spaces
+    .section    .rodata
+_spaces:
+    .string "                "
+    .data
+    .align 8
+    .type   ss, @object
+    .size   ss, 8
 spaces:
-    .asciz "                "
-
-.text
+    .quad   _spaces
+    .text
 .globl asma
-.globl _asma
-
-asma: _asma:
-# code start
-    enter $0, $0
+    .type   asma_orig, @function
+asma:
+    .cfi_startproc
+    enter 0, 0
 
     movdqa (%rdi), %xmm8
-    pcmpeqb spaces(%rip), %xmm8
-    xor %rax, %rax
+    pcmpeqb _spaces(%rip), %xmm8
     pmovmskb %xmm8, %eax
     popcntl %eax, %eax
 
     leave
     ret
-# code end
+
+    .size   asma_orig, .-asma_orig
+    .ident  "GCC: (Debian 4.4.5-8) 4.4.5"
+    .section    .note.GNU-stack,"",@progbits
